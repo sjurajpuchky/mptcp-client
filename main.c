c1d578d0e618be04f1c84b5f03adffc9d7836770
Juraj Puchký <info@baba.bj>
2019-03-28 18:32:17 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jpuchky
+ *
+ * Created on 14. března 2019, 18:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define SOL_TCP 6
+#define MPTCP_ENABLED 42
+
+void error(const char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    int enable = 1;
+    setsockopt(sockfd, SOL_TCP, MPTCP_ENABLED, &enable, sizeof (enable));
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof (serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr,
+            (char *) &serv_addr.sin_addr.s_addr,
+            server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
+        error("ERROR connecting");
+    printf("Start sending data...");
+    while (1) {
+        bzero(buffer, 256);
+
+        n = write(sockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR writing to socket");
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR reading from socket");
+    }
+    close(sockfd);
+    return 0;
+}
